94056d0b9f049cc318774b4fcff5ec13db4bfd2e
Claus Due <claus@wildside.dk>
2013-02-17 12:41:54 +0100

    [TASK] Implement Flux autoloader

--- ext_autoload.php
+++ ext_autoload.php
@@ -1,8 +1,3 @@
 <?php
-$extensionClassesPath = t3lib_extMgm::extPath('notify') . 'Classes/';
-return array(
-	'tx_notify_scheduler_abstractschedulertask' => $extensionClassesPath . 'Scheduler/AbstractSchedulerTask.php',
-	'tx_notify_scheduler_notificationschedulertask' => $extensionClassesPath . 'Scheduler/NotificationSchedulerTask.php',
-	'tx_notify_scheduler_sharedfieldprovider' => $extensionClassesPath . 'Scheduler/SharedFieldProvider.php',
-);
-?>
+require_once t3lib_extMgm::extPath('flux', 'Classes/Utility/Autoload.php');
+return Tx_Flux_Utility_Autoload::getAutoloadRegistryForExtension('notify');
